1737fc9f25ff8c0e481c37e74a58abdb0f727dc5
Luca de Alfaro <luca@dealfaro.org>
2016-11-07 21:57:23 -0800

    Primitive search bar display, and vue.js start

---
+++ static/js/default_index.js
@@ -0,0 +1,52 @@
+// This is the js for the default/index.html view.
+
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false; // show all warnings
+
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    // Enumerates an array.
+    var enumerate = function(v) {
+        var k=0;
+        return v.map(function(e) {e._idx = k++;});
+    };
+
+    self.get_products = function () {
+        // Gets new products in response to a query, or to an initial page load.
+    };
+
+
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+            product_list: [],
+            cart: []
+        },
+        methods: {
+            get_products: self.get_products()
+        }
+
+    });
+
+    self.get_products();
+    $("#vue-div").show();
+
+
+    return self;
+};
+
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
